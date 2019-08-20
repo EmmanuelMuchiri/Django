16a8731dd828350e31de2e80a70509624c72bdb2
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-20 15:45:46 +0300

    added try and catch error

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -37,7 +37,13 @@ def convert_dates(dates):
 
 def past_days_news(request, past_date):
     # Converts data from the string Url
-    date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
 
     day = convert_dates(date)
     html = f'''
