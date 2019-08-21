d1d272b9e17e70e4d2f840654b830606f45f9fa8
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-21 13:32:05 +0300

    added work

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from .models import Editor,Article,tags
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.emmanuel= Editor(first_name = 'Emmanuel', last_name ='Muchiri', email ='emmanuel.muchiri@outlook.com')
+
+    def test__instance(self):
+        self.assertEquals(self.emmanuel.first_name,"Emmanuel")
+
+    def test_save_method(self):
+        self.emmanuel.save_editor()
+        editors=Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+    
+    def test_delete(self):
+        self.emmanuel.delete_editor()
+        
+        
