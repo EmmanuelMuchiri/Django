6c84b02a7959ee486f0100142616a930f7daed8b
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-21 16:25:50 +0300

    tests working perfectly

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 
@@ -29,4 +30,16 @@ class Article(models.Model):
     tags = models.ManyToManyField(tags)
     editor = models.ForeignKey(Editor,on_delete = models.DO_NOTHING)
     pub_date = models.DateTimeField(auto_now_add=True,blank =True)
+
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+    
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
 
--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Editor,Article,tags
+import datetime as dt
 # Create your tests here.
 
 class EditorTestClass(TestCase):
@@ -15,7 +16,34 @@ class EditorTestClass(TestCase):
         editors=Editor.objects.all()
         self.assertTrue(len(editors)>0)
     
-    def test_delete(self):
-        self.emmanuel.delete_editor()
-        
+    # def test_delete(self):
+    #     self.emmanuel.delete_editor()
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.emmanuel= Editor(first_name = 'Emmanuel', last_name ='Muchiri', email ='emmanuel.muchiri@outlook.com')
+        self.emmanuel.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.emmanuel)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
+
         
