639f4ed906f2e29fb6b87dc9e3a712a5e3863dc6
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-21 12:10:48 +0300

    added column pub_date

--- news/models.py
+++ news/models.py
@@ -9,3 +9,19 @@ class Editor(models.Model):
 
 def __str__(self):
     return self.first_name
+class Meta:
+    ordering = ['first_name']
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    tags = models.ManyToManyField(tags)
+    editor = models.ForeignKey(Editor,on_delete = models.DO_NOTHING)
+    pub_date = models.DateTimeField(auto_now_add=True,blank =True)
+
