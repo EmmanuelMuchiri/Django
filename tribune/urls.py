99fe684de20ff64133471936ac86866d2ef82e21
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-28 17:56:17 +0300

    login logout working perfectly

--- tribune/urls.py
+++ tribune/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views 
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls'))
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}) 
 ]
 
