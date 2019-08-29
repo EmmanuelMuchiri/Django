bafb5cb07246347c3fe177b9600475334fee3283
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-29 13:13:25 +0300

    nothing worked

--- news/models.py
+++ news/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 
@@ -10,12 +12,12 @@ class Editor(models.Model):
     phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
-        return self.first_name
-    class Meta:
-        ordering = ['first_name']
+    #     return self.first_name
+        class Meta:
+            ordering = ['first_name']
 
-    def save_editor(self):
-        self.save()
+        def save_editor(self):
+            self.save()
     
 
 class tags(models.Model):
@@ -25,12 +27,13 @@ class tags(models.Model):
         return self.name
 
 class Article(models.Model):
-    title = models.CharField(max_length =60)
-    post = models.TextField()
+    title = models.CharField(max_length=60)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
-    editor = models.ForeignKey(Editor,on_delete = models.DO_NOTHING)
-    pub_date = models.DateTimeField(auto_now_add=True,blank =True)
-    article_image = models.ImageField(upload_to= 'articles/',default="article_image")
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to='articles/', blank=True)
+
 
 
     @classmethod
--- tribune/settings.py
+++ tribune/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tinymce',
     'news',
     'bootstrap3',
     'django.contrib.admin',
